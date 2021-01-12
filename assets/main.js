9a7132e629b20eff68a8e983d32666582936276f
Zachary M <31627113+zmontgo@users.noreply.github.com>
2021-01-12 15:19:27 -0600

    Add smooth scroll

--- assets/main.js
+++ assets/main.js
@@ -90,3 +90,44 @@ window.onload = function() {
     css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
     document.body.appendChild(css);
 };
+
+function scrollPageTo (to, duration=500) {
+  //t = current time
+  //b = start value
+  //c = change in value
+  //d = duration
+  const easeInOutQuad = function (t, b, c, d) {
+    t /= d/2;
+    if (t < 1) return c/2*t*t + b;
+    t--;
+    return -c/2 * (t*(t-2) - 1) + b;
+  };
+
+  return new Promise((resolve, reject) => {
+    const element = document.scrollingElement;
+
+    if (typeof to === 'string') {
+      to = document.querySelector(to) || reject();
+    }
+    if (typeof to !== 'number') {
+      to = to.getBoundingClientRect().top + element.scrollTop;
+    }
+
+    let start = element.scrollTop,
+        change = to - start,
+        currentTime = 0,
+        increment = 20;
+
+    const animateScroll = function() {
+        currentTime += increment;
+        let val = easeInOutQuad(currentTime, start, change, duration);
+        element.scrollTop = val;
+        if(currentTime < duration) {
+            setTimeout(animateScroll, increment);
+        } else {
+          resolve();
+        }
+    };
+    animateScroll();
+  });
+}
