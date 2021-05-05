9ded8030c31928ce34f17dddcbbcceed3cb380be
Zachary M <31627113+zmontgo@users.noreply.github.com>
2021-05-05 09:27:57 -0500

    Update main.js

--- assets/main.js
+++ assets/main.js
@@ -2,7 +2,8 @@ const DARK_THEME = '(prefers-color-scheme: dark)'
 const LIGHT_THEME = '(prefers-color-scheme: light)'
 
 function changeWebsiteTheme(scheme) {
-  console.log(scheme);
+  const scheme = e.matches ? 'dark' : 'light';
+  
   if (scheme === 'dark') {
     root.style.setProperty('--background-color', "#222");
     root.style.setProperty('--text-color', "#fff");
@@ -18,21 +19,8 @@ function detectColorScheme() {
     if(!window.matchMedia) {
       return
     }
-    function listener({matches, media}) {
-        if(!matches) { // Not matching anymore = not interesting
-          return
-        }
-        if(media === DARK_THEME) {
-          console.log('Bad')
-          changeWebsiteTheme('dark')
-        } else if (media === LIGHT_THEME) {
-          changeWebsiteTheme('light')
-        }
-    }
-    const mqDark = window.matchMedia(DARK_THEME)
-    mqDark.addListener(listener)
-    const mqLight = window.matchMedia(LIGHT_THEME)
-    mqLight.addListener(listener)
+    const mqDark = window.matchMedia(DARK_THEME);
+    mqDark.addEventListener('change', changeWebsiteTheme());
 }
 
-detectColorScheme()
+changeWebsitetheme();
