337a9611c87f7cddba339ac0c2316b9fd2df5182
Zachary Montgomery <31627113+zmontgo@users.noreply.github.com>
2021-05-05 07:44:11 -0500

    Add dark theme framework

--- assets/main.js
+++ assets/main.js
@@ -1,155 +1,38 @@
-const header = document.querySelector('.site-header');
-const toggler = document.querySelector('.menu-icon');
-const spacer = document.querySelector('.topnav-spacer');
-const body = document.getElementById('body');
-const selector = document.querySelector('input[name=menu-btn]');
-let root = document.documentElement;
+const DARK_THEME = '(prefers-color-scheme: dark)'
+const LIGHT_THEME = '(prefers-color-scheme: light)'
 
-function scrollCheck() {
-    const scrollPos = window.scrollY;
-    if (body.classList.contains('scrolled') != true) {body.classList.add('scrolled');}
-    if (scrollPos > 10) {
-        header.classList.add('scrolled');
-        if (spacer != null) spacer.classList.add('scroll');
-    } else {
-        header.classList.remove('scrolled');
-        if (spacer != null) spacer.classList.remove('scroll');
-    }
+function changeWebsiteTheme(scheme) {
+  console.log(scheme);
+  if (scheme === 'dark') {
+    root.style.setProperty('--background-color', "#222");
+    root.style.setProperty('--text-color', "#fff");
+    root.style.setProperty('--text-grey-color', "#999");
+  } else {
+    root.style.setProperty('--background-color', "#fff");
+    root.style.setProperty('--text-color', "#000");
+    root.style.setProperty('--text-grey-color', "#666");
+  }
 }
 
-window.addEventListener('scroll', () => {
-    scrollCheck();
-});
-
-;(function(){
-    function addAnim() {
-        if (toggler.classList.contains('animate') != true) {
-            toggler.classList.add('animate');
+function detectColorScheme() {
+    if(!window.matchMedia) {
+      return
+    }
+    function listener({matches, media}) {
+        if(!matches) { // Not matching anymore = not interesting
+          return
         }
-
-        if (body.style.overflow == "hidden") {body.style.overflow = "";}
-        else if (body.style.overflow == "" && selector.checked != true) {body.style.overflow = "hidden";}
-    };
-
-    // listen to mouseover for the container
-    toggler.addEventListener('click', addAnim);
-})();
-
-var TxtType = function(el, toRotate, period) {
-    this.toRotate = toRotate;
-    this.el = el;
-    this.loopNum = 0;
-    this.period = parseInt(period, 10) || 2000;
-    this.txt = '';
-    this.tick();
-    this.isDeleting = false;
-};
-
-TxtType.prototype.tick = function() {
-    var i = this.loopNum % this.toRotate.length;
-    var fullTxt = this.toRotate[i];
-
-    if (this.isDeleting) {
-    this.txt = fullTxt.substring(0, this.txt.length - 1);
-    } else {
-    this.txt = fullTxt.substring(0, this.txt.length + 1);
-    }
-
-    this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
-
-    var that = this;
-    var delta = 200 - Math.random() * 100;
-
-    if (this.isDeleting) { delta /= 2; }
-
-    if (!this.isDeleting && this.txt === fullTxt) {
-    delta = this.period;
-    this.isDeleting = true;
-    } else if (this.isDeleting && this.txt === '') {
-    this.isDeleting = false;
-    this.loopNum++;
-    delta = 500;
-    }
-
-    setTimeout(function() {
-    that.tick();
-    }, delta);
-};
-
-window.onload = function() {
-    var elements = document.getElementsByClassName('typewrite');
-    for (var i=0; i<elements.length; i++) {
-        var toRotate = elements[i].getAttribute('data-type');
-        var period = elements[i].getAttribute('data-period');
-        if (toRotate) {
-          new TxtType(elements[i], JSON.parse(toRotate), period);
+        if(media === DARK_THEME) {
+          console.log('Bad')
+          changeWebsiteTheme('dark')
+        } else if (media === LIGHT_THEME) {
+          changeWebsiteTheme('light')
         }
     }
-    // INJECT CSS
-    var css = document.createElement("style");
-    css.type = "text/css";
-    css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
-    document.body.appendChild(css);
-};
-
-function scrollPageTo (to, duration=500) {
-  //t = current time
-  //b = start value
-  //c = change in value
-  //d = duration
-  const easeInOutQuad = function (t, b, c, d) {
-    t /= d/2;
-    if (t < 1) return c/2*t*t + b;
-    t--;
-    return -c/2 * (t*(t-2) - 1) + b;
-  };
-
-  return new Promise((resolve, reject) => {
-    const element = document.scrollingElement;
-
-    if (typeof to === 'string') {
-      to = document.querySelector(to) || reject();
-    }
-    if (typeof to !== 'number') {
-      to = to.getBoundingClientRect().top + element.scrollTop;
-    }
-
-    let start = element.scrollTop,
-        change = to - start,
-        currentTime = 0,
-        increment = 20;
-
-    const animateScroll = function() {
-        currentTime += increment;
-        let val = easeInOutQuad(currentTime, start, change, duration);
-        element.scrollTop = val;
-        if(currentTime < duration) {
-            setTimeout(animateScroll, increment);
-        } else {
-          resolve();
-        }
-    };
-    animateScroll();
-  });
+    const mqDark = window.matchMedia(DARK_THEME)
+    mqDark.addListener(listener)
+    const mqLight = window.matchMedia(LIGHT_THEME)
+    mqLight.addListener(listener)
 }
 
-//root.style.setProperty('--gradient-dark', "#181122");
-//root.style.setProperty('--gradient-light', "#1b2132");
-
-function copyToClipboard(text) {
-  var dummy = document.createElement("textarea");
-  // to avoid breaking orgain page when copying more words
-  // cant copy when adding below this code
-  // dummy.style.display = 'none'
-  document.body.appendChild(dummy);
-  //Be careful if you use texarea. setAttribute('value', value), which works with "input" does not work with "textarea". – Eduard
-  dummy.value = text;
-  dummy.select();
-  document.execCommand("copy");
-  document.body.removeChild(dummy);
-}
-
-function uncheck() {
-  var checkbox = document.getElementById('checkbox');
-  setTimeout(function(){ if (document.getElementById('checkbox').checked == true) {document.getElementById('checkbox').checked = false;} }, 1000);
-}
+detectColorScheme()
